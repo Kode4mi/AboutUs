3fd23b06ab8c0a78b767f9a239721500109dc9ae
Deznka <jan.slepaczuk@gmail.com>
2023-10-07 18:51:42 +0200

    Changes in project section

--- src/components/AboutUsCard.jsx
+++ src/components/AboutUsCard.jsx
@@ -1,16 +1,16 @@
 'use client'
 import 'animate.css';
-import {useEffect, useState} from 'react';
-import {PortableText} from "@portabletext/react";
+import { useEffect, useState } from 'react';
+import { PortableText } from "@portabletext/react";
 import newImg from '../assets/default-image.webp';
-import {urlFor} from "../utils/sanity-client.ts";
+import { urlFor } from "../utils/sanity-client.ts";
 
-function Card({active, data}) {
-    const [cardClasses, setCardClasses] = useState("card lg:card-side bg-slate-700 shadow-xl w-1/3 min-w-[300px] h-fit animate__animated ");
+function Card({ active, data }) {
+    const [cardClasses, setCardClasses] = useState("card lg:card-side bg-slate-700 shadow-xl w-1/2 min-w-[300px] h-fit animate__animated ");
     const [cardData, setCardData] = useState({});
 
     useEffect(() => {
-        setCardClasses("card lg:card-side bg-slate-700 shadow-xl w-1/3 min-w-[300px] h-fit animate__animated animate__fadeOutDown")
+        setCardClasses("card lg:card-side bg-slate-700 shadow-xl w-1/2 min-w-[300px] h-fit animate__animated animate__fadeOutDown")
         setTimeout(() => {
             setCardClasses(cardClasses + " animate__fadeInDown");
             setCardData(data.find((card) => {
@@ -22,11 +22,11 @@ function Card({active, data}) {
     return <>
         {cardData && <div className={cardClasses}>
             <figure className="h-[250px] lg:h-full lg:w-1/3"><img alt={cardData.name} src={(cardData.image && urlFor(cardData.image)) ?? newImg.src}
-                                                                  className="h-full lg:object-cover lg:h-full"/></figure>
+                className="h-full lg:object-cover lg:h-full" /></figure>
             <div className="card-body lg:w-2/3 h-fit ">
 
-                <h1 className="card-title text-2xl">{cardData.name}</h1>
-                <div><PortableText value={cardData.description}/></div>
+                <h1 className="card-title text-4xl">{cardData.name}</h1>
+                <div className="text-2xl"><PortableText value={cardData.description} /></div>
 
             </div>
         </div>}
--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,14 +1,43 @@
-// ProjectList.js
 import React, { useEffect, useState } from 'react';
 import { urlFor } from "../../utils/sanity-client.ts";
 
 const ProjectList = ({ data }) => {
 
+    const [selectedImage, setSelectedImage] = useState(0);
+
+    // temporary images for testing
+    const images = [
+        "https://i.kym-cdn.com/photos/images/original/002/283/888/620.jpg",
+        "https://i.kym-cdn.com/photos/images/original/002/507/353/92b.jpg",
+        "https://i.insider.com/6340783eb3e94d0019781b4e?width=700"
+    ]
+
+    const imageChange = (direction) => {
+        if (direction === "previous") {
+            selectedImage === 0 ? setSelectedImage(2) : setSelectedImage(selectedImage - 1);
+        }
+        else {
+            selectedImage === 2 ? setSelectedImage(1) : setSelectedImage(selectedImage + 1);
+        }
+    }
 
     return (
-        <div className="flex flex-col">
-            <h1 className={`p-2 mb-2 text-3xl font-extrabold`}>{data.title}</h1>
-            <div className="h-[300px] border-[1px] border-solid border-red-600"></div>
+        <div className="mb-10">
+            <h1 className="p-2 mb-2 text-3xl font-extrabold">{data.title}</h1>
+            <div className="flex flex-col lg:flex-row border-[1px] border-solid border-red-600 relative">
+                <div className="lg:w-2/3 h-40 lg:h-auto border-[1px] border-dashed border-red-600 relative">
+                    <p className="pl-10">{data.slug.current}</p>
+                </div>
+                <div className="carousel w-full lg:w-1/3">
+                    <div id="slide1" className="carousel-item relative w-full">
+                        <img src={images[selectedImage]} className="w-full" />
+                        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                            <a className="btn btn-circle" onClick={() => imageChange("previous")}>❮</a>
+                            <a className="btn btn-circle" onClick={() => imageChange("next")}>❯</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
